48b9ba6b18cc6cfbab2a8ecdb5097e129c3f7be4
Daniel.Li <121407764@qq.com>
2024-05-09 22:08:17 +0800

    add test controller

---
+++ springboot-jdbc-demo/src/main/java/org/example/springbootjdbcdemo/controller/TestController.java
@@ -0,0 +1,92 @@
+package org.example.springbootjdbcdemo.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.springbootjdbcdemo.common.DatabaseConnectionManager;
+import org.example.springbootjdbcdemo.dao.BookRepository;
+import org.example.springbootjdbcdemo.dao.UserRepository;
+import org.example.springbootjdbcdemo.entity.Book;
+import org.example.springbootjdbcdemo.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class TestController {
+    @Autowired
+    BookRepository bookRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    DatabaseConnectionManager databaseConnectionManager;
+
+    private ObjectMapper MAPPER = new ObjectMapper();
+
+    @RequestMapping(path = "/api/test", method = RequestMethod.GET)
+    public String test() throws SQLException {
+        System.out.println("test jdbc()");
+
+        Connection connection = null;
+        try {
+            connection = databaseConnectionManager.getConnection();
+            userRepository.setConnection(connection);
+            bookRepository.setConnection(connection);
+
+            User user1 = new User();
+            int id = userRepository.getMaxId() + 1;
+            System.out.println("id: " + id);
+            user1.setUserId(id);
+            user1.setUserName("daniel" + id);
+            userRepository.save(user1);
+
+            // Add book
+            Book book1 = new Book();
+            UUID uuid = UUID.randomUUID();
+            String bookId = uuid.toString().substring(0, 8);
+            System.out.println("bookId: " + bookId);
+            book1.setBookId(bookId);
+            book1.setBookName("test" + bookId);
+            Map<String, Object> map = new HashMap<>();
+            map.put("bookId", "11");
+            map.put("bookName", "test");
+            book1.setOrigin(MAPPER.writeValueAsString(map));
+            bookRepository.save(book1);
+
+            book1.setCreateBy("daniel");
+            bookRepository.update(book1);
+
+            // Query book
+            List<Book> books = bookRepository.findAll();
+            System.out.println(books.size());
+
+            // Delete book
+//            bookDao.deleteByPrimaryKey(bookId);
+//            bookRepository.delete(book1);
+
+//        }
+//        catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("#### data is rollback");
+            connection.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            return "DONE";
+        }
+    }
+}
